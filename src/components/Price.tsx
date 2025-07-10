1b9d6f61e91de99746051c4d9f52dfded7a6fd5f
Sakshikumariii <sakshismp2003@gmail.com>
2025-07-10 12:58:20 +0530

    update

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -1,26 +1,28 @@
 "use client";
-
+ 
 import React, { useEffect, useState } from "react";
-
+ 
 type Props = {
   price: number;
+  id: number;
   options?: { title: string; additionalPrice: number }[];
 };
-
+ 
 const Price = ({ price, options }: Props) => {
   const [total, setTotal] = useState(price);
   const [quantity, setQuantity] = useState(1);
   const [selected, setSelected] = useState(0);
-
+ 
   useEffect(() => {
     setTotal(
       quantity * (options ? price + options[selected].additionalPrice : price)
     );
   }, [quantity, selected, options, price]);
-
+ 
   return (
     <div className="flex flex-col gap-4">
       <h2 className="text-2xl font-bold">${total.toFixed(2)}</h2>
+     
       <div className="flex gap-4">
         {options?.map((option, index) => (
           <button
@@ -36,7 +38,9 @@ const Price = ({ price, options }: Props) => {
           </button>
         ))}
       </div>
+   
       <div className="flex justify-between items-center">
+       
         <div className="flex justify-between w-full p-3 ring-1 ring-red-500">
           <span>Quantity</span>
           <div className="flex gap-4 items-center">
@@ -53,6 +57,7 @@ const Price = ({ price, options }: Props) => {
             </button>
           </div>
         </div>
+     
         <button className="uppercase w-56 bg-red-500 text-white p-3 ring-1 ring-red-500">
           Add to Cart
         </button>
@@ -60,5 +65,5 @@ const Price = ({ price, options }: Props) => {
     </div>
   );
 };
-
+ 
 export default Price;
